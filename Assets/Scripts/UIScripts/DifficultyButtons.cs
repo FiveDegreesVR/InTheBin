b9e890af43ce2939aa60944fe5167442fd13993f
WizardFromTheJungle <65914126+WizardFromTheJungle@users.noreply.github.com>
2024-02-11 11:46:28 -0500

    UI

---
+++ Assets/Scripts/UIScripts/DifficultyButtons.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //dont show up unless prompted
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Controls()
+    {
+        //switch case to different games based on difficulty
+    }
+}
