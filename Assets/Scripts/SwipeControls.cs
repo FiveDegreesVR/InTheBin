fd83383be9aba8fb9732a94dd5df1c612ff05a6c
Nikita Smith <65087157+FiveDegreesVR@users.noreply.github.com>
2024-02-11 20:37:24 -0500

    Fixed Swipe Controls

--- Assets/Scripts/SwipeControls.cs
+++ Assets/Scripts/SwipeControls.cs
@@ -6,6 +6,9 @@ public class SwipeControls : MonoBehaviour
 {
     public float speed;
     public GameObject trashcan;
+    private bool moving = false;
+    private Vector3 initTouch;
+    private Vector3 endTouch;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +22,29 @@ public class SwipeControls : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
-
+            
+            /*
             if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
             {
                 // get the touch position from the screen touch to world point
                 Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, 0.0f, 0.0f));
                 // lerp and set the position of the current object to that of the touch, but smoothly over time.
                 trashcan.transform.position = Vector3.Lerp(trashcan.transform.position, touchedPos * speed, Time.deltaTime);
+            }
+            */
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                initTouch = Camera.main.ScreenToViewportPoint(new Vector3(touch.position.x, touch.position.y, 0.0f));
+                print(initTouch);
+            }
+            
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endTouch = Camera.main.ScreenToViewportPoint(new Vector3(touch.position.x, touch.position.y, 0.0f));
+                print(endTouch);
+                
+                trashcan.GetComponent<Rigidbody>().AddForce((endTouch-initTouch) * speed, ForceMode.Impulse);
             }
         }
     }
