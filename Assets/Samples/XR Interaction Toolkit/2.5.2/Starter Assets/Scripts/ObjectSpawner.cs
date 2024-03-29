aced93ca09ec4a68d9227fb31c1e4b8175a2cbf8
Nonutt1n <116680294+Nonutt1n@users.noreply.github.com>
2024-03-29 17:41:23 -0400

    Revert "Added_AR_DropSystem"
    This reverts commit 6013fccc88075001e2e53b2aee975b3097400c23.

--- Assets/Samples/XR Interaction Toolkit/2.5.2/Starter Assets/Scripts/ObjectSpawner.cs
+++ Assets/Samples/XR Interaction Toolkit/2.5.2/Starter Assets/Scripts/ObjectSpawner.cs
@@ -9,10 +9,6 @@ namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
     /// </summary>
     public class ObjectSpawner : MonoBehaviour
     {
-
-        private bool already_spawned = false;
-        //this boolean if checking if the object has spawned yet,
-        //once it has then it will not place another object.
         [SerializeField]
         [Tooltip("The camera that objects will face when spawned. If not set, defaults to the main camera.")]
         Camera m_CameraToFace;
@@ -196,54 +192,46 @@ namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
         /// <seealso cref="objectSpawned"/>
         public bool TrySpawnObject(Vector3 spawnPoint, Vector3 spawnNormal)
         {
-            if (!already_spawned)
+            if (m_OnlySpawnInView)
             {
-                if (m_OnlySpawnInView)
+                var inViewMin = m_ViewportPeriphery;
+                var inViewMax = 1f - m_ViewportPeriphery;
+                var pointInViewportSpace = cameraToFace.WorldToViewportPoint(spawnPoint);
+                if (pointInViewportSpace.z < 0f || pointInViewportSpace.x > inViewMax || pointInViewportSpace.x < inViewMin ||
+                    pointInViewportSpace.y > inViewMax || pointInViewportSpace.y < inViewMin)
                 {
-                    var inViewMin = m_ViewportPeriphery;
-                    var inViewMax = 1f - m_ViewportPeriphery;
-                    var pointInViewportSpace = cameraToFace.WorldToViewportPoint(spawnPoint);
-                    if (pointInViewportSpace.z < 0f || pointInViewportSpace.x > inViewMax || pointInViewportSpace.x < inViewMin ||
-                        pointInViewportSpace.y > inViewMax || pointInViewportSpace.y < inViewMin)
-                    {
-                        return false;
-                    }
+                    return false;
                 }
-
-                var objectIndex = isSpawnOptionRandomized ? Random.Range(0, m_ObjectPrefabs.Count) : m_SpawnOptionIndex;
-                var newObject = Instantiate(m_ObjectPrefabs[objectIndex]);
-                if (m_SpawnAsChildren)
-                    newObject.transform.parent = transform;
-
-                newObject.transform.position = spawnPoint;
-                EnsureFacingCamera();
-
-                var facePosition = m_CameraToFace.transform.position;
-                var forward = facePosition - spawnPoint;
-                BurstMathUtility.ProjectOnPlane(forward, spawnNormal, out var projectedForward);
-                newObject.transform.rotation = Quaternion.LookRotation(projectedForward, spawnNormal);
-
-                if (m_ApplyRandomAngleAtSpawn)
-                {
-                    var randomRotation = Random.Range(-m_SpawnAngleRange, m_SpawnAngleRange);
-                    newObject.transform.Rotate(Vector3.up, randomRotation);
-                }
-
-                if (m_SpawnVisualizationPrefab != null)
-                {
-                    var visualizationTrans = Instantiate(m_SpawnVisualizationPrefab).transform;
-                    visualizationTrans.position = spawnPoint;
-                    visualizationTrans.rotation = newObject.transform.rotation;
-                }
-
-                objectSpawned?.Invoke(newObject);
-                already_spawned = true;
-                return true;
             }
-            else
+
+            var objectIndex = isSpawnOptionRandomized ? Random.Range(0, m_ObjectPrefabs.Count) : m_SpawnOptionIndex;
+            var newObject = Instantiate(m_ObjectPrefabs[objectIndex]);
+            if (m_SpawnAsChildren)
+                newObject.transform.parent = transform;
+
+            newObject.transform.position = spawnPoint;
+            EnsureFacingCamera();
+                
+            var facePosition = m_CameraToFace.transform.position;
+            var forward = facePosition - spawnPoint;
+            BurstMathUtility.ProjectOnPlane(forward, spawnNormal, out var projectedForward);
+            newObject.transform.rotation = Quaternion.LookRotation(projectedForward, spawnNormal);
+
+            if (m_ApplyRandomAngleAtSpawn)
             {
-                return true;
+                var randomRotation = Random.Range(-m_SpawnAngleRange, m_SpawnAngleRange);
+                newObject.transform.Rotate(Vector3.up, randomRotation);
             }
+
+            if (m_SpawnVisualizationPrefab != null)
+            {
+                var visualizationTrans = Instantiate(m_SpawnVisualizationPrefab).transform;
+                visualizationTrans.position = spawnPoint;
+                visualizationTrans.rotation = newObject.transform.rotation;
+            }
+
+            objectSpawned?.Invoke(newObject);
+            return true;
         }
     }
 }
