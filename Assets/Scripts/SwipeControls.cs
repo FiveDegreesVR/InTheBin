4b77d8b1109d445f0ca9b22c8f3915270a36678f
Nikita Smith <65087157+FiveDegreesVR@users.noreply.github.com>
2024-04-01 07:35:03 -0400

    Undid swipe changes

--- Assets/Scripts/SwipeControls.cs
+++ Assets/Scripts/SwipeControls.cs
@@ -1,24 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO.Pipes;
 using UnityEngine;
 
 public class SwipeControls : MonoBehaviour
 {
     public float speed;
     public GameObject trashcan;
-    public float minSwipeDistance;
-    public float maxSwipeDistance;
-    public float dragSpeed;
-
+    private bool moving = false;
+    private Vector3 initTouch;
+    private Vector3 endTouch;
     private Rigidbody _rigidbody;
-    private bool isSwiping = false;
-    private bool isChangingDirection = false;
-    private Vector2 swipeStartPosition;
-    private Vector2 lastTouchPosition;
-    private float swipeDirection;
-    private float swipeDistance;
-    private Vector3 swipeVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -30,66 +21,30 @@ public class SwipeControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DetectSwipe();
-    }
-    void DetectSwipe()
-    {
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0);
-
-            switch (touch.phase)
+            Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
+            
+            /*
+            if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
             {
-                case TouchPhase.Began:
-                    isSwiping = true;
-                    swipeStartPosition = touch.position;
-                    lastTouchPosition = swipeStartPosition;
-                    swipeDistance = 0f;
-                    swipeDirection = 0f;
-                    isChangingDirection = false;
-                    break;
-                case TouchPhase.Moved:
-                    if (isSwiping)
-                    {
-                        Vector2 touchDelta = touch.position - lastTouchPosition;
-                        Vector3 move = new Vector3(touchDelta.x, 0, 0) * dragSpeed * Time.deltaTime;
-                        trashcan.transform.Translate(move, Space.World);
-                        lastTouchPosition = touch.position;
-                        swipeDistance += Mathf.Abs(touchDelta.x);
-                        if (swipeDirection == 0f)
-                        {
-                            swipeDirection = Mathf.Sign(touchDelta.x);
-                            swipeVelocity = move / Time.deltaTime;
-                        }
-                        else if (Mathf.Sign(touchDelta.x) != swipeDirection && !isChangingDirection)
-                        {
-                            // If direction changes while actively swiping, reset the swipe velocity
-                            swipeVelocity = Vector3.zero;
-                            isChangingDirection = true;
-                        }
-                    }
-                    break;
-                case TouchPhase.Stationary:
-                    if (isSwiping)
-                    {
-                        Vector3 move = swipeVelocity * Time.deltaTime;
-                        trashcan.transform.Translate(move, Space.World);
-                    }
-                    break;
-                case TouchPhase.Ended:
-                    isSwiping = false;
-                    float swipeSpeed = Mathf.Clamp(swipeDistance / maxSwipeDistance, 0f, 1f);
-                    Vector3 finalMove = new Vector3(swipeDirection, 0, 0) * swipeSpeed * dragSpeed;
-                    _rigidbody.velocity = finalMove;
-                    break;
+                // get the touch position from the screen touch to world point
+                Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, 0.0f, 0.0f));
+                // lerp and set the position of the current object to that of the touch, but smoothly over time.
+                trashcan.transform.position = Vector3.Lerp(trashcan.transform.position, touchedPos * speed, Time.deltaTime);
             }
-        }
-        else
-        {
-            // If touch is not active, stop the trashcan movement
-            _rigidbody.velocity = Vector3.zero;
-            isSwiping = false;
-            isChangingDirection = false;
+            */
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                initTouch = Camera.main.ScreenToViewportPoint(new Vector3(touch.position.x, touch.position.y, 0.0f));
+            }
+            
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endTouch = Camera.main.ScreenToViewportPoint(new Vector3(touch.position.x, touch.position.y, 0.0f));
+                _rigidbody.AddForce((endTouch-initTouch) * speed, ForceMode.Impulse);
+            }
         }
     }
 }
